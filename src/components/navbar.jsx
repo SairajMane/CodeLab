7d163d6742d096c99a842bd570e0e910288df71e
Sairaj Mane <sairajmane123ab@gmail.com>
2023-03-19 04:05:56 +0530

    Init

---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+class Navbar extends Component {
+
+    state = { 
+        logo : "",
+        username : "",
+        mail : ""
+    }
+
+    render() { 
+        return (
+            <div className="nav">
+                <div className="logo">
+
+                </div>
+                <div className="user-details">
+
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Navbar;
