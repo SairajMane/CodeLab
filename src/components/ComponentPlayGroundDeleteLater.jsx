7154430cdeafcd635e120308ea3c9900fc39b36c
Sairaj Mane <sairajmane123ab@gmail.com>
2023-04-26 23:29:32 +0530

    Assignment Dummy Service added

--- src/components/ComponentPlayGroundDeleteLater.jsx
+++ src/components/ComponentPlayGroundDeleteLater.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import Assignment from './assignment';
-import Lab from './lab';
+import React, { Component } from "react";
+import Assignment from "./assignment";
+import Lab from "./lab";
 
 class ComponentPlayGround extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <div className='viewport'>
-                <Lab/>
-                <Assignment/>
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <Assignment />
+      </div>
+    );
+  }
 }
- 
+
 export default ComponentPlayGround;
