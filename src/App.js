959f30a57deabb76e95ebf04d02611ef1b0daec0
Siddhant <sonawane2582@gmail.com>
2023-05-20 10:52:16 +0530

    Changes(createlab,createassignment,regoptions)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import Navbar from './components/navbar';
-import Testsidebar from './components/Testsidebar';
-import Prblmstmt from './components/Prblmstmt';
+import Sidebar from './components/Sidebar';
+import ProblemStatement from './components/ProblemStatement';
 import Dropdown from './components/Dropdownmenu';
+import LabCreate from './components/createLab';
+import AssignmentCreate from './components/createAssignment';
+import RegOptions from './pages/regOptions';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Prblmstmt />
+      <AssignmentCreate />
     </div>
   );
 }
